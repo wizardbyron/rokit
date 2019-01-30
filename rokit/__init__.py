9ecfcfa9a221cb96dfcc13306090fd9add54c723
wizardbyron <wizardbyron@icloud.com>
2019-01-30 12:54:25 +0800

    Reorgnize package.

--- rokit/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import absolute_import
-from .cli import cli
-
-if __name__ == "__main__":
-    cli()
---
+++ rokit/__main__.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import
+from .cli import cli
+
+if __name__ == "__main__":
+    cli()
